c70882f6c5b31cd1fb5c6b403bd2ad6bb98a9e31
G. Wade Johnson <gwadej@anomaly.org>
2013-07-01 23:32:17 -0500

    Modify list command to use a supplied filehandle.

--- lib/App/TimelogTxt/Stack.pm
+++ lib/App/TimelogTxt/Stack.pm
@@ -89,10 +89,11 @@ sub _readline_pos
 
 sub list
 {
-    my ($self) = @_;
+    my ($self, $ofh) = @_;
+    $ofh ||= \*STDOUT;
     return unless -f $self->{file};
     open my $fh, '<', $self->{file};
-    print reverse <$fh>;
+    print {$ofh} reverse <$fh>;
     return;
 }
 
@@ -101,13 +102,11 @@ __END__
 
 =head1 NAME
 
-ModName - [One line description of module's purpose here]
-
+App::TimelogTxt::Stack - Interface to the stack file for the timelog application.
 
 =head1 VERSION
 
-This document describes ModName version 0.03
-
+This document describes App::TimelogTxt::Stack version 0.03_1
 
 =head1 SYNOPSIS
 
@@ -117,8 +116,8 @@ This document describes ModName version 0.03
     Brief code example(s) here showing commonest usage(s).
     This section will be as far as many users bother reading
     so make it as educational and exeplary as possible.
-  
-  
+
+
 =head1 DESCRIPTION
 
 =for author to fill in:
@@ -126,98 +125,46 @@ This document describes ModName version 0.03
     Use subsections (=head2, =head3) as appropriate.
 
 
-=head1 INTERFACE 
+=head1 INTERFACE
 
-=for author to fill in:
-    Write a separate section listing the public components of the modules
-    interface. These normally consist of either subroutines that may be
-    exported, or methods that may be called on objects belonging to the
-    classes provided by the module.
+=head2 new( $filename )
 
+=head2 $s->clear()
 
-=head1 DIAGNOSTICS
+=head2 $s->push( $task )
 
-=for author to fill in:
-    List every single error and warning message that the module can
-    generate (even the ones that will "never happen"), with a full
-    explanation of each problem, one or more likely causes, and any
-    suggested remedies.
+=head2 $task = $s->pop()
 
-=over
+=head2 $s->drop( $arg )
 
-=item C<< Error message here, perhaps with %s placeholders >>
-
-[Description of error here]
-
-=item C<< Another error message here >>
-
-[Description of error here]
-
-[Et cetera, et cetera]
-
-=back
-
+=head2 $s->list( $fh )
 
 =head1 CONFIGURATION AND ENVIRONMENT
 
-=for author to fill in:
-    A full explanation of any configuration system(s) used by the
-    module, including the names and locations of any configuration
-    files, and the meaning of any environment variables or properties
-    that can be set. These descriptions must also include details of any
-    configuration language used.
-  
-ModName requires no configuration files or environment variables.
-
+App::TimelogTxt::Stack requires no configuration files or environment variables.
 
 =head1 DEPENDENCIES
 
-=for author to fill in:
-    A list of all the other modules that this module relies upon,
-    including any restrictions on versions, and an indication whether
-    the module is part of the standard Perl distribution, part of the
-    module's distribution, or must be installed separately. ]
-
-None.
-
+autodie.
 
 =head1 INCOMPATIBILITIES
 
-=for author to fill in:
-    A list of any modules that this module cannot be used in conjunction
-    with. This may be due to name conflicts in the interface, or
-    competition for system or program resources, or due to internal
-    limitations of Perl (for example, many modules that use source code
-    filters are mutually incompatible).
-
 None reported.
 
-
 =head1 BUGS AND LIMITATIONS
-
-=for author to fill in:
-    A list of known problems with the module, together with some
-    indication Whether they are likely to be fixed in an upcoming
-    release. Also a list of restrictions on the features the module
-    does provide: data types that cannot be handled, performance issues
-    and the circumstances in which they may arise, practical
-    limitations on the size of data sets, special cases that are not
-    (yet) handled, etc.
 
 No bugs have been reported.
 
 =head1 AUTHOR
 
-G. Wade Johnson  C<< wade@anomaly.org >>
-
+G. Wade Johnson  C<< gwadej@cpan.org >>
 
 =head1 LICENCE AND COPYRIGHT
 
-Copyright (c) <YEAR>, G. Wade Johnson C<< wade@anomaly.org >>. All rights reserved.
+Copyright (c) 2013, G. Wade Johnson C<< gwadej@cpan.org >>. All rights reserved.
 
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself. See L<perlartistic>.
-
 
 =head1 DISCLAIMER OF WARRANTY
 
